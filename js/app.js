113754bc78115b19b9683adb4bb74785e4315751
Melanie Archer <job@twobanjos.com>
2014-01-26 15:51:19 -0800

    Got SunCalc working with Moment.js (though with hard-coded value for lat/long).

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
 window.onload = function(){
-    var  dateDisplay = document.getElementById('now');
-    console.log(calculateTimesOfDay('America/Los_Angeles'));
+    var  dateDisplay = document.getElementById('now'),
+         place = 'Los Angeles, California, USA';
+    calculateTimesOfDay('America/Los_Angeles');
 }();
+// TODO: calculate quarter-hour opacity changes
 
 function calculateMyTime(place){
   // use moment.js to create appropriate time for
   // `place`
-  var rightNow = moment.tz(new Date(), place).format();
+  var rightNow = moment.tz(new Date(), place);
   return rightNow;
+}
+
+function calculateLatLong(place){
+  // use Google Maps API to obtain lat/long of `place`
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ 'address': place }, function(results, status){
+    var location = results[0].geometry.location;
+    return [location.lat(), location.lng()];
+  });
 }
 
 // use SunCalc library to create sunrise, noon, sunset values
 function calculateTimesOfDay(place){
-  var now = calculateMyTime(place);
-  var times = SunCalc.getTimes();
-  var sunrise = times.sunrise.getHours();
-  return sunrise;
+  var now = calculateMyTime(place),
+      latLong = calculateLatLong(place),
+      times;
+
+  // TODO: this keeps coming back undefined. It's just sloooow--proper location is
+  // coming back from Google Maps, eventually
+  if (latLong){
+    times = SunCalc.getTimes(now, latLong[0], latLong[1]);
+  }
+  // hard-code lat/long for LA
+  else {
+    times = SunCalc.getTimes(now, 34.0522342, -118.2436849 );
+  }
+  var sunrise = times.sunrise,
+      sunset = times.sunset,
+      noon = times.solarNoon;
+  console.log(noon);
 }
